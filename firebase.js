565b50caf6362791e2b03303a99e8c840182d6f2
Laure <nilugette@yahoo.fr>
2021-08-30 14:47:48 +0200

    Google authentication with Firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBkb4cjDbXChsvRNxsnaAr-BNdjZE56xKs",
+    authDomain: "amzn-b90ef.firebaseapp.com",
+    projectId: "amzn-b90ef",
+    storageBucket: "amzn-b90ef.appspot.com",
+    messagingSenderId: "654382577506",
+    appId: "1:654382577506:web:625f0ea3907fad7cfe9e3b",
+    measurementId: "G-32TBTCPSHK"
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import Image from "next/image"
 import { MenuIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from "next-auth/client"
 
 function Header() {
+const [session] = useSession()
+
     return (
         <header>
             <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
@@ -20,15 +23,15 @@ function Header() {
                     <SearchIcon className="h-12 p-4" />
                 </div>
                 <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-                    <div className="link">
-                        <p>Hello Laure</p>
+                    <div onClick={!session ? signIn : signOut} className="cursor-pointer link">
+                        <p>{session ? `Hello, ${session.user.name}` : 'Sign In' }</p>
                         <p className="font-extrabold md:text-sm">Account & List</p>
                     </div>
-                    <div className="link">
+                    <div className="cursor-pointer link">
                         <p>Returns</p>
                         <p className="font-extrabold md:text-sm">& Orders</p>
                     </div>
-                    <div className="relative link flex items-center">
+                    <div className="relative link cursor-pointer flex items-center">
                         <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
                         <ShoppingCartIcon className="h-10" />
                         <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
